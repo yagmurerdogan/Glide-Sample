341f307ed2727b0daf09e99f876ab0d0a66b936f
Yagmur Erdogan <yagmur.erdogan.1999@gmail.com>
2021-03-08 10:18:41 +0300

    Create second activity

---
+++ app/src/main/java/com/yagmurerdogan/glidedemo/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.yagmurerdogan.glidedemo
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
